457b7cbd7741dfbe4a94f3d6946821813670585c
maana <maana@maanasas-air.wireless-1x.unc.edu>
2019-08-25 17:46:25 -0400

    Added code to A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,27 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int numOfCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			String firstName = scan.next();
+			String firstInitial = firstName.charAt(0) + ".";
+			String lastName = scan.next();
+			int totalCartItems = scan.nextInt();
+			double totalPrice = 0.0;
+			
+			for (int j = 0; j < totalCartItems; j++) {
+				double itemTotalPrice = 0.0;
+				int numOfItems = scan.nextInt();
+				String itemName = scan.next();
+				double itemPrice = scan.nextDouble();
+				for (int k = 0; k < numOfItems; k++) {
+					itemTotalPrice += itemPrice;
+				}
+				totalPrice += itemTotalPrice;
+			}
+			System.out.println(firstInitial + " " + lastName  + ": " + totalPrice);
+		}
+		scan.close();
 	}
 }
