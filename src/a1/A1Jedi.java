111af8fb71f9316df0671310a93a965b6a40ce69
maana <maana@maanasas-air.wireless-1x.unc.edu>
2019-08-30 13:59:12 -0400

    Jedi finished

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,8 +7,46 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int numOfItems = scan.nextInt();
+		String[] shopItems = new String[numOfItems];
+		double[] prices = new double[numOfItems];
+		
+		for (int i = 0; i < numOfItems; i++) {
+			shopItems[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		
+		int numOfCust = scan.nextInt();
+		String[] custName = new String[numOfCust];
+		int numOfItem = 0;
+		String nameOfItem = "";
+		int[] numOfBought = new int[numOfItems];
+		int[] numOfBuyers = new int[numOfItems];
+		
+		for (int i = 0; i < numOfCust; i++) {
+			custName[i] = scan.next() + " " + scan.next();
+			int totalBought = scan.nextInt();
+			for (int k = 0; k < totalBought; k++) {
+				numOfItem = scan.nextInt();
+				nameOfItem = scan.next();
+				for (int s = 0; s < shopItems.length; s++) {
+					if (nameOfItem.equals(shopItems[s])) {
+						numOfBought[s] += numOfItem;
+						numOfBuyers[s] += 1;
+					}
+				}
+			}
+		}
+		
+		scan.close();
+		
+		for (int m = 0; m < numOfItems; m++) {
+			if (numOfBought[m] == 0 || numOfBuyers[m] == 0) {
+				System.out.println("No customers bought " + shopItems[m]);
+			} else {
+				System.out.println (numOfBuyers[m] + " customers bought " + numOfBought[m] + " " + shopItems[m]);
+			}
+		}
 		
 	}
 }
